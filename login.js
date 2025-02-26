1ec4da2c7f9e8934856b21f4f893a44843a36350
JavierrT-dev <tamezromanluisjavier@gmail.com>
2025-02-26 01:26:00 +0000

    1

--- login.js
+++ login.js
@@ -5,7 +5,7 @@ document.getElementById("loginForm").addEventListener("submit", async function (
   const password = document.getElementById("password").value;
 
   try {
-    const response = await fetch("http://localhost:3000/login", {
+    const response = await fetch("https://ubiquitous-space-orbit-7vp7666w4q5vhp7rv-3000.app.github.dev/login.html", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
